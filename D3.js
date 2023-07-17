ec40bad4e84a7208002ec5b07943272970c1ba1f
AndreaFerrara13 <134604849+AndreaFerrara13@users.noreply.github.com>
2023-07-17 08:40:54 +0200

    js

--- D3.js
+++ D3.js
@@ -197,16 +197,48 @@ console.log(crewMass);
 
   Una volta fatto, modifica la massa di qualche elemento dell'equipaggio e vedi se riesci ad ottenere un messaggio diverso.
 */
-
+if (crewMass <= 500) {
+  console.log("Ship is under loaded");
+} else if (crewMass > 500 && crewMass <= 700) {
+  console.log("Ship is half loaded");
+} else if (crewMass > 700 && crewMass <= 900) {
+  console.log("Warning: Load is over 700");
+} else if (crewMass > 900 && crewMass <= 1000) {
+  console.log("Critical Load: Over 900");
+} else if (crewMass > 1000) {
+  console.log("DANGER! OVERLOAD ALERT: escape from ship now!");
+} else {
+  console.log("Crew mass value not recognized");
+}
 /* ESERCIZIO 8
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
 */
-
+for (let = i; i < starWarsCharacters.length; i++) {
+  const currentCharacter = starWarsCharacters[i];
+  if (currentCharacter.gender === n / a) {
+    currentCharacter.gender = "robot";
+  }
+}
+console.log(starWarsCharacters);
 /* --EXTRA-- ESERCIZIO 9
   Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "characters" le stringhe corrispondenti a personaggi con lo stesso nome.
   Una volta fatto crea un console.log per controllare la proprietà length di "characters" prima e dopo l'operazione.
 */
+console.log("CHARACTERS BEFORE", characters.length);
 
+for (let i = 0; i < characters.length; i++) {
+  const currentCharacter = characters[i];
+
+  for (let j = 0; j < femaleCharacters.length; j++) {
+    const currentFemaleCharacter = femaleCharacters[j];
+
+    if (currentFemaleCharacter.name === currentCharacter) {
+      characters.splice(i, 1);
+    }
+  }
+}
+
+console.log("CHARACTERS AFTER", characters.length);
 /* --EXTRA-- ESERCIZIO 10
   Crea una funzionalità che selezioni un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo (a tuo piacimento).
 */
